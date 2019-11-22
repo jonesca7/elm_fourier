6a1fe0aaa10fe7ea68ddaa504946432d37e017af
Lucas Shanks <lucassshanks@gmail.com>
2019-11-21 19:23:12 -0500

    committed red circle

--- src/Main.elm
+++ src/Main.elm
@@ -4,13 +4,37 @@ import Html exposing (..)
 import Bootstrap.CDN as CDN
 import Bootstrap.Grid as Grid
 
+import Array exposing (..)
+import GraphicSVG exposing (..)
+import GraphicSVG.EllieApp exposing (..)
+import List
+import ShapeCreateAssets exposing (..)
+
 
 main =
-    Grid.container []
-        [ CDN.stylesheet -- creates an inline style node with the Bootstrap CSS
-        , Grid.row []
-            [ Grid.col []
-                [ text "Some content for my view here..."]
-            ]
+    gameApp Tick
+        { model = init -- init is the value in the field model
+        , title = "Shape Creator"
+        , view = view
+        , update = update
+        }
 
-        ]
+
+init =
+    { currentPage = 1
+    }
+
+
+view model =
+    collage 500 500
+    [
+        circle 10 |> filled red
+    ]
+
+
+update msg model =
+        { model | currentPage = 2}
+
+type Msg m1
+    = Tick Float GetKeyState
+    | In1
