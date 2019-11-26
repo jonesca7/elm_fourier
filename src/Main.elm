2106c8c3889bc0d97620da9e9bc251f8da5b5d3b
Cam Van Ravens <camvanravens@outlook.com>
2019-11-26 18:34:36 -0500

    Added signifiers, finished layout, added other stuff

--- src/Main.elm
+++ src/Main.elm
@@ -120,8 +120,8 @@ main =
 init =
     { currentPage = 1,
     amplitude = 1,
-    maxAmplitude = 5,
-    minAmplitude = 0.5,
+    maxAmplitude = 2.5,
+    minAmplitude = 0.1,
     frequency = 1,
     maxFrequency = 10,
     minFrequency = 0.1,
@@ -129,6 +129,7 @@ init =
     maxPhase = 1.9,
     sampleCount = 50,
     waves = [],
+    maxWaves = 6,
     graphComponent = "real"
     }
 
@@ -136,6 +137,57 @@ init =
 view model =
     collage 1000 500 <|
         [
+            GraphicSVG.text "Frequency Domain" |> size 12 |> centered |> filled black |> move (240, 200),
+            group [
+                polygon [ ( -6, 0 ), ( 6, 0 ), ( 0, 12 ) ] |> filled red |> move (54, 12) |> notifyTap SamplingUp,
+                polygon [ ( -6, 0 ), ( 6, 0 ), ( 0, -12 ) ] |> filled red |> move (54, -6) |> notifyTap SamplingDown,
+                text ("Number of Samples (N) = " ++ String.fromInt model.sampleCount) |> size 10 |> centered |> filled black |> move (0, 0)
+            ] |> move (200, -175),
+            group [
+                text "Show: " |> size 10 |> centered |> filled black |> move (0, 0),
+                text "Real" |> size 10 |> centered |> (if model.graphComponent == "real" then filled black else filled grey) |> move (30, 0) |> notifyTap SetGraphReal,
+                text "Imag" |> size 10 |> centered |> (if model.graphComponent == "imag" then filled black else filled grey) |> move (60, 0) |> notifyTap SetGraphImag
+            ] |> move (320, -175),
+            barChart (getComplexComponent model (computeDFT (getSampledInput model.sampleCount model.waves))) |> move (240, 50),
+            GraphicSVG.line (0, 0) (waveGraphWidth + 60, 0) |> outlined (solid 1) grey |> move (-510, 125)
+        ]
+        ++ -- Draw each of the wave components
+        (List.Extra.indexedFoldl(\i w a ->
+            a
+            ++
+            [
+                GraphicSVG.line (0, 0) (waveGraphWidth, 0) |> outlined (dotted 1) grey |> move (-480, 100 - (toFloat i * waveHeight)),
+                GraphicSVG.line (0, 0) (waveGraphWidth + 60, 0) |> outlined (solid 1) grey |> move (-510, 75 - (toFloat i * waveHeight)),
+                GraphicSVG.circle 10 |> filled red |> makeTransparent 0.2 |> addOutline (solid 1) red |> move (0, 100 - (toFloat i * waveHeight)),
+                text "🗑" |> size 12 |> filled red |> move (-4, 96 - (toFloat i * waveHeight)) -- |> NotifyTap somehow remove this specific wave
+            ]
+            ++
+            (List.map(\x -> 
+              let
+                p1 = {x = toFloat x, y = w.amp * sin(w.freq * toFloat x * xStep + (w.phase * pi)) * waveScaleY}
+                p2 = {x = toFloat x + 1, y = w.amp * sin(w.freq * (toFloat x + 1) * xStep + (w.phase * pi)) * waveScaleY}
+              in
+                GraphicSVG.line (p1.x, p1.y) (p2.x, p2.y) |> outlined (solid 1) black |> move (-480, 100 - (toFloat i * waveHeight))
+            ) <| List.range 0 waveGraphWidth)
+          ) [] model.waves
+        )
+        ++ -- Draw the combination wave at the top
+        [
+            GraphicSVG.line (0, 0) (waveGraphWidth, 0) |> outlined (dotted 1) grey |> move (-480, 200)
+        ]
+        ++
+        (List.map (\x ->
+          let
+            p1 = {x = toFloat x, y = (getSumWave model.waves (toFloat x * xStep)) * waveScaleY}
+            p2 = {x = toFloat x + 1, y = (getSumWave model.waves ((toFloat x + 1) * xStep)) * waveScaleY}
+          in
+            GraphicSVG.line (p1.x, p1.y) (p2.x, p2.y) |> outlined (solid 1) black |> move (-480, 200)
+          ) <| List.range 0 (waveGraphWidth - 1)
+        )
+        ++
+        [
+            --GraphicSVG.line (0, 0) (0, 500) |> outlined (solid 1) grey |> move (0, -250),
+            GraphicSVG.rect 500 80 |> filled white |> move (-252, -217),
             group [
                 polygon [ ( -6, 0 ), ( 6, 0 ), ( 0, 12 ) ] |> filled red |> move (-5, 12) |> notifyTap AmplitudeUp,
                 polygon [ ( -6, 0 ), ( 6, 0 ), ( 0, -12 ) ] |> filled red |> move (-5, -6) |> notifyTap AmplitudeDown,
@@ -147,45 +199,16 @@ view model =
                 text (String.fromFloat model.amplitude ++ " sin " ) |> size 10 |> centered |> filled black |> move (0, 0),
                 text ("( " ++ String.fromFloat model.frequency ++ "x + ") |> size 10 |> centered |> filled black |> move (30, 0),
                 text (String.fromFloat model.phase ++ "𝜋)") |> size 10 |> centered |> filled black |> move (57, 0)
-            ] |> move (-270, -180),
+            ] |> move (-320, -210),
             group [
-                rect 60 20 |> filled blue |> move (0, 0) |> notifyTap AddWave,
+                rect 60 20 |> (if List.length model.waves == model.maxWaves then filled grey else filled blue) |> move (0, 0) |> notifyTap AddWave,
                 text "Add wave" |> sansserif |> centered |> filled white |> move (0, -4) |> notifyTap AddWave
-            ] |> move (-150, -180),
+            ] |> move (-200, -207),
             group [
-                rect 60 20 |> filled red |> move (0, 0) |> notifyTap Reset,
+                rect 60 20 |> (if List.length model.waves == 0 then filled grey else filled red) |> move (0, 0) |> notifyTap Reset,
                 text "Reset" |> sansserif |> centered |> filled white |> move (0, -4) |> notifyTap Reset
-            ] |> move (-80, -180),
-            GraphicSVG.text "Frequency Domain" |> size 12 |> centered |> filled black |> move (240, 200),
-          group [
-            polygon [ ( -6, 0 ), ( 6, 0 ), ( 0, 12 ) ] |> filled red |> move (54, 12) |> notifyTap SamplingUp,
-            polygon [ ( -6, 0 ), ( 6, 0 ), ( 0, -12 ) ] |> filled red |> move (54, -6) |> notifyTap SamplingDown,
-            GraphicSVG.text ("Number of Samples (N) = " ++ String.fromInt model.sampleCount) |> size 10 |> centered |> filled black |> move (0, 0)
-          ] |> move (200, -175),
-          barChart (getComplexComponent model (computeDFT (getSampledInput model.sampleCount model.waves)))
-            |> move (240, 50) 
+            ] |> move (-80, -207)
         ]
-        ++ -- Draw each of the wave components
-        (List.Extra.indexedFoldl(\i w a ->
-            a ++ 
-            (List.map(\x -> 
-              let
-                p1 = {x = toFloat x, y = w.amp * sin(w.freq * toFloat x * xStep + (w.phase * pi)) * waveScaleY}
-                p2 = {x = toFloat x + 1, y = w.amp * sin(w.freq * (toFloat x + 1) * xStep + (w.phase * pi)) * waveScaleY}
-              in
-                GraphicSVG.line (p1.x, p1.y) (p2.x, p2.y) |> outlined (solid 1) black |> move (-480, 100 - (toFloat i * waveHeight))
-            ) <| List.range 0 waveGraphWidth)
-          ) [] model.waves
-        )
-        ++ -- Draw the combination wave at the top
-        (List.map (\x ->
-          let
-            p1 = {x = toFloat x, y = (getSumWave model.waves (toFloat x * xStep)) * waveScaleY}
-            p2 = {x = toFloat x + 1, y = (getSumWave model.waves ((toFloat x + 1) * xStep)) * waveScaleY}
-          in
-            GraphicSVG.line (p1.x, p1.y) (p2.x, p2.y) |> outlined (solid 1) black |> move (-480, 200)
-          ) <| List.range 0 (waveGraphWidth - 1)
-        )
 
 
 update msg model =
@@ -196,14 +219,14 @@ update msg model =
         AmplitudeUp ->
             { model | amplitude = 
                 if model.amplitude < model.maxAmplitude then
-                    model.amplitude + 0.5
+                    Maybe.withDefault 0 (String.toFloat (Round.round 1 (model.amplitude + 0.1)))
                 else
                     model.amplitude 
             }
         AmplitudeDown ->
             { model | amplitude = 
                 if model.amplitude > model.minAmplitude then
-                    model.amplitude - 0.5
+                    Maybe.withDefault 0 (String.toFloat (Round.round 1 (model.amplitude - 0.1))) 
                 else
                     model.amplitude 
             }
@@ -226,7 +249,7 @@ update msg model =
                 if model.phase < model.maxPhase then
                     Maybe.withDefault 0 (String.toFloat (Round.round 1 (model.phase + 0.1)))
                 else
-                    0.0 
+                    0.0
             }
         PhaseDown ->
             { model | phase =
@@ -237,7 +260,10 @@ update msg model =
             }
         AddWave ->
             { model | waves =
-                model.waves ++ [{amp = model.amplitude, freq = model.frequency, phase = model.phase}]
+                if List.length model.waves < model.maxWaves then
+                    model.waves ++ [{amp = model.amplitude, freq = model.frequency, phase = model.phase}]
+                else
+                    model.waves
             }
         SamplingUp ->
             { model | sampleCount = 
@@ -255,6 +281,10 @@ update msg model =
             }
         Reset ->
             { model | waves = [] }
+        SetGraphReal ->
+            { model | graphComponent = "real" }
+        SetGraphImag ->
+            { model | graphComponent = "imag" }
 
 type alias Wave =
     {
@@ -281,3 +311,5 @@ type Msg m1
     | SamplingUp
     | SamplingDown
     | Reset
+    | SetGraphReal
+    | SetGraphImag
